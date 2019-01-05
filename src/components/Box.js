6aa2724aa940e4a83b3f6d62375809afcd0f6806
Jeo D <e8gea6w5z8id@opayq.com>
2019-01-05 11:16:05 -0700

    Refactor Demo render method and refactor Box component

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,33 +3,9 @@ import '../styles/main.scss';
 
 class Box extends Component {
   render() {
-    const { id, index , i, action, boxIds } = this.props;
-    const shouldSwap = boxIds[i] > boxIds[i + 1];
-    let [divClass, iClass, jClass] = ['', '', ''];
-    if (action === 'swap' && shouldSwap) {
-      iClass = 'right-swap';
-      jClass = 'left-swap';
-    } else if (action === 'compare' && shouldSwap) {
-      iClass = 'unsorted';
-      jClass= 'unsorted';
-    } else if (action === 'compare' && !shouldSwap) {
-      iClass = 'sorted';
-      jClass= 'sorted';
-    }
-    switch (index) {
-      case i:
-        divClass = `Box ${iClass} Box-underlined`;
-        break;
-      case i + 1:
-        divClass = `Box ${jClass}`;
-        break;
-      default:
-        divClass = 'Box';
-        break;
-    }
     return (
-      <div className={divClass}>
-        <span className="Box--id">{id}</span>
+      <div className={`Box ${this.props.divClass}`} >
+        <span className="Box--id">{this.props.id}</span>
       </div>
     );
   }
--- src/components/Demo.js
+++ src/components/Demo.js
@@ -21,6 +21,38 @@ class Demo extends Component {
     }
   }
 
+  getBoxes = () => {
+    const { i, iteration, boxIds, action } = this.state;
+    const shouldSwap = boxIds[i] > boxIds[i + 1];
+    let [iClass, jClass] = this.getClassNames(action, shouldSwap);
+    return boxIds.map((num, index) => {
+      let divClass = 'Box ';
+      if (index >= boxIds.length - iteration + 1) {
+        divClass = 'Box Box-final-position';
+      } else if (index === i) {
+        divClass += iClass;
+      } else if (index === i + 1) {
+        divClass += jClass;
+      }
+      return <Box id={num} divClass={divClass} key={num}/>;
+    });
+  }
+
+  getClassNames = (action, shouldSwap) => {
+    let iClass, jClass;
+    if (action === 'swap' && shouldSwap) {
+      iClass = 'right-swap';
+      jClass = 'left-swap';
+    } else if (action === 'compare' && shouldSwap) {
+      iClass = 'unsorted';
+      jClass= 'unsorted';
+    } else if (action === 'compare' && !shouldSwap) {
+      iClass = 'sorted';
+      jClass= 'sorted';
+    }
+    return [iClass, jClass];
+  }
+
   startAlgorithm = () => {
     this.setState({
       iteration: 1,
@@ -31,9 +63,12 @@ class Demo extends Component {
   goForward = () => {
     const { i, iteration, boxIds, action } = this.state;
     const n = boxIds.length - 1;
-    const shouldSwap = boxIds[i] > boxIds[i + 1]
-    if (iteration === n) {
-      this.setState({ action: '' });
+    const shouldSwap = boxIds[i] > boxIds[i + 1];
+    if (iteration >= n) {
+      this.setState({
+        action: '',
+        iteration: boxIds.length + 1
+      });
     } else if (i === n - iteration && action === 'swap') {
       const newBoxIds = this.swapNumbers(boxIds, i);
       this.setState({
@@ -64,26 +99,21 @@ class Demo extends Component {
   }
 
   render() {
-    const { boxIds, i , iteration, action } = this.state;
+    const { boxIds, iteration } = this.state;
     const showStartButton = iteration === 0 ? true : false;
-    const randomBoxes = boxIds.map((num, index) => {
-      return (
-        <Box
-          id={num}
-          index={index}
-          i={i}
-          action={action}
-          boxIds={boxIds}
-          key={num}
-        />
-      );
-    });
+    const randomBoxes = this.getBoxes();
     return (
       <section className="Demo">
         <h2 className="Demo--h2">Bubble Sort</h2>
         <p>explanation of step</p>
         <div className="algorithm">
           {randomBoxes}
+          {boxIds.map((num, index) => {
+            const spanClass = index === this.state.i ?
+              'algorithm--span algorithm--span-underline' : 'algorithm--span';
+            return <span id={index} className={spanClass} key={num}></span>
+          })
+          }
         </div>
           {showStartButton && 
             <footer className="footer">
