05c09c0811e10ec46ff13c0ece3ba8eb91dcc8b0
Jeo D <e8gea6w5z8id@opayq.com>
2019-01-06 09:29:16 -0700

    Add snapshot test for Box component

--- src/tests/Box.test.js
+++ src/tests/Box.test.js
@@ -5,7 +5,7 @@ import { shallow } from 'enzyme';
 describe('Box', () => {
   it('should match the snapshot', () => {
     let wrapper = shallow(
-      <Box id="1" divClass="Box" key="0" />
+      <Box id={1} divClass="Box" key={0} />
     );
     expect(wrapper).toMatchSnapshot();
   });
