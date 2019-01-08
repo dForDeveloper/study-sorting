7d3f18ab2f7d1cc036ad4ae1cd597aace5b5b4c6
Jeo D <e8gea6w5z8id@opayq.com>
2019-01-08 10:09:47 -0700

    Add tests for App component

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('it should have default state', () => {
+    expect(wrapper.state()).toEqual({ showBubbleSort: true });
+  });
+  
+  it('it should set showBubbleSort to false when goToInsertionSort is called',
+  () => {
+    expect(wrapper.state()).toEqual({ showBubbleSort: true });
+    wrapper.instance().goToInsertionSort();
+    expect(wrapper.state()).toEqual({ showBubbleSort: false });
+  });
+  
+  it('it should set showBubbleSort to true when goToBubbleSort is called',
+  () => {
+    expect(wrapper.state()).toEqual({ showBubbleSort: true });
+    wrapper.instance().goToInsertionSort();
+    expect(wrapper.state()).toEqual({ showBubbleSort: false });
+    wrapper.instance().goToBubbleSort();
+    expect(wrapper.state()).toEqual({ showBubbleSort: true });
+  });
 });
